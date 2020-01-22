eea0ed979309d97951df4c8a270522201535bc2b
juergen-geinitz <geinitz@denic.de>
2020-01-22 09:17:15 +0100

    updating for dell idrac

---
+++ firfoxjavaselector.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+tmp_file=$(tempfile 2>/dev/null) || tmp_file=/tmp/test$$
+trap "rm -f $tmp_file" 0 1 2 5 15
+
+setjavato () {
+	case $1 in
+	6)
+		L=/usr/java/jdk1.6.0_45/jre/lib/i386/
+		;;
+	7)
+		L=/usr/java/jdk1.7.0_80/jre/lib/i386/
+		;;
+	8)
+		L=/usr/java/jre1.8.0_131/lib/i386/
+		;;
+	esac
+	/bin/rm /usr/lib/mozilla/plugins/libnpjp2.so
+	ln -s ${L}/libnpjp2.so /usr/lib/mozilla/plugins
+}
+#
+dialog --menu "Firefox java version plugin selector" \
+	15 72 \
+	3 \
+	"6" "java version 6" \
+	"7" "java version 7" \
+	"8" "java version 8" \
+	2> $tmp_file
+case $? in
+	0)
+		setjavato $(cat $tmp_file)
+		;;
+	1)	echo "cancelled"
+		;;
+	255)
+		echo "ESC pressed"
+		;;
+	*)
+		echo "error"
+		;;
+esac
+
+firefox &
